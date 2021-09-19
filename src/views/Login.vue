bcff22930879a97dcaa68556078fd5a9b68045fd
Camilla Dilling-Hansen <camilla@dilling.co>
2021-09-19 14:16:46 +0200

    Initial installation clean up

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Ji
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
